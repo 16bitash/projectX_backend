ff91ec47cce9900f14fa590977c21a1d411c9b01
Ashish Kuamr <entropy.ash@gmail.com>
2018-02-09 22:15:59 +0530

    Add some database

--- database/modles.js
+++ database/modles.js
@@ -9,36 +9,35 @@ const db = new Sequelize(DB.DATABASE, DB.USER, DB.PASSWORD, {
 });
 
 const user = db.define('users', {
-    phone: {
+    userId: {
         type: datatypes.INTEGER,
         primaryKey: true,
+        autoIncrement: true
     },
     name: {
         type: datatypes.STRING,
         allowNull: false,
     },
+    phone: {
+        type: datatypes.INTEGER,
+        unique: true,
+        allowNull: false
+    },
     email: {
         type: datatypes.STRING,
-        unique: true,
-        allowNull: false,
+        unique: true
     },
     password: {
         // Implement Hashing
         type: datatypes.STRING,
         allowNull: false,
     },
-    address1: {
+    address: {
         type: datatypes.STRING
     },
-    address2: {
-        type: datatypes.STRING
-    },
-    address3: {
-        type: datatypes.STRING
-    },
-    cridits: {
+    respect: {
         type: datatypes.INTEGER,
-        primaryKey: true,
+        defaultValue: 0
     },
     isPopular: {
         type: datatypes.BOOLEAN,
@@ -47,3 +46,69 @@ const user = db.define('users', {
 }, {
     freezeTableName: true
 });
+
+const order = db.define('orders', {
+    userId: {
+        type: datatypes.INTEGER,
+        foreignKey: true,
+    },
+    orderId: {
+        type: datatypes.STRING,
+        allowNull: false,
+    },
+    timeOfOrder: {
+        type: datatypes.DATE,
+        defaultValue: datatypes.DATE,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true
+});
+
+
+const orderDetails = db.define('orders', {
+    userId: {
+        type: datatypes.INTEGER,
+        allowNull: false,
+    },
+    orderId: {
+        type: datatypes.STRING,
+        allowNull: false,
+    },
+    timeOfOrder: {
+        type: datatypes.DATE,
+        defaultValue: Sequelize.NOW,
+    },
+    designId: {
+        type: datatypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true
+});
+
+const design = db.define('orders', {
+    designId: {
+        type: datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    color: {
+        type: datatypes.STRING,
+    },
+    imagesAndDetails: {
+        type: datatypes.TEXT,
+    },
+    textAndDetails: {
+        type: datatypes.TEXT,
+    },
+}, {
+    freezeTableName: true
+});
+
+Promise.all([
+    user.sync(),
+    order.sync(),
+    orderDetails.sync(),
+    design.sync()
+]).then(() => console.log('Database connected!'))
